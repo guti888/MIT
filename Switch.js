e46c65d7db994ec91506124095f0eb89f83643e0
guti888 <86857107+guti888@users.noreply.github.com>
2021-07-22 20:24:58 -0700

    Add files via upload

---
+++ Switch.js
@@ -0,0 +1,11 @@
+var example = 1;
+var condition;
+condition = example > 120;
+switch(condition){
+    case true:
+    //execute code instructions
+    break;
+    case false:
+    //execute code instructions
+    break;
+}
